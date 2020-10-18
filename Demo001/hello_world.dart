0b61998171188052078c35cbe7605503841f4660
techleiws <techliujing@gmail.com>
2020-10-18 19:41:36 +0800

    Update hello_world.dart
    Load Sky X files on iOS where available

--- Demo001/hello_world.dart
+++ Demo001/hello_world.dart
@@ -1,8 +1,11 @@
+//library hello_world;
 import 'package:sky/widgets/basic.dart';
 
 class HelloWorldApp extends App {
   Widget build() {
     return new Text('Hello, world!');
+    //Center(child: new Text('Hello, world!'))
+    // DSL布局方式的改变
   }
 }
 

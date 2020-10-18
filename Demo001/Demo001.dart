adf64babf3455199d7d1b383cfc45a83537d8b8a
techleiws <techliujing@gmail.com>
2020-10-18 11:16:01 +0800

    Update Demo001.dart

--- Demo001/Demo001.dart
+++ Demo001/Demo001.dart
@@ -2,5 +2,21 @@
 import "dart:math";
 import 'dart:sky';
 void main() {
-    print("Hello, world");
+  print("Hello, world");
+
+  double width = 500.0;
+  double height = 500.0;
+
+  PictureRecorder recorder = new PictureRecorder(width, height);
+  double radius = min(width, height) * 0.45;
+
+  Paint paint = new Paint()..setARGB(255, 0, 255, 0);
+
+  recorder.drawCircle(width / 2, height / 2, radius, paint);
+
+  print("Storing picture");
+  view.picture = recorder.endRecording();
+
+  print("Scheduling paint");
+  view.schedulePaint();
 }

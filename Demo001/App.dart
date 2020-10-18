6ac61e50cd3fb3228fccc1a3ec8405e43f395e8e
techleiws <techliujing@gmail.com>
2020-10-18 11:11:46 +0800

    0327
    Node class 是构建UI的工具类

---
+++ Demo001/App.dart
@@ -0,0 +1,5 @@
+// material design 倒入widget包
+import './hello_world.dart';
+void main() {
+  runApp(new HelloWorldApp());
+}
---
+++ Demo001/hello_world.dart
@@ -0,0 +1,7 @@
+import 'package:sky/framework/fn.dart';
+
+class HelloWorldApp extends App {
+  Node build() {
+    return new Text('Hello, world!');
+  }
+}

bb2e423cf4e8d2ca1a17ab57f1f655536cd86584
liujing <techliujing@gmail.com>
2020-10-20 00:10:46 +0800

    rename

---
+++ Demo001/SwingDemo.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+class Untitled {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(110, 110);
+		frame.setVisible(true);
+		frame.setTitle("你好");
+	}
+}
